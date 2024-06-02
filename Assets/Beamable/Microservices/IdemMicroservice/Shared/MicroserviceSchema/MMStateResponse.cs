5cddb2fed4bfa05450d0c9d7d752287dba6f4d75
d.fofanov <dmitry.fofanov@roamergames.com>
2024-06-02 14:08:42 +0500

    Microservice rename Idem -> IdemMicroservice

---
+++ Assets/Beamable/Microservices/IdemMicroservice/Shared/MicroserviceSchema/MMStateResponse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Beamable.Microservices.Idem.Shared.MicroserviceSchema
+{
+    public class MMStateResponse : BaseResponse
+    {
+        public bool inQueue;
+        public bool matchFound;
+        public bool timeout;
+        public string gameMode;
+        public string matchId;
+        public IdemPlayer[] players;
+
+
+        public MMStateResponse(bool inQueue, bool matchFound, bool timeout, string gameMode = "", string matchId = "", IdemPlayer[] players = null) : base(true)
+        {
+            this.inQueue = inQueue;
+            this.matchFound = matchFound;
+            this.timeout = timeout;
+            this.gameMode = gameMode;
+            this.matchId = matchId;
+            this.players = players;
+        }
+
+        public static MMStateResponse None() => new(false, false, false);
+        public static MMStateResponse InQueue(string gameMode) => new(true, false, false, gameMode);
+
+        public static MMStateResponse MatchFound(string gameMode, string matchId, IEnumerable<IdemPlayer> players)
+            => new(false, true, false, gameMode, matchId, players.ToArray());
+        
+        public static MMStateResponse Timeout() => new(false, false, true);
+    }
+}
---
+++ Assets/Beamable/Microservices/IdemMicroservice/Shared/MicroserviceSchema/StringResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Beamable.Microservices.Idem.Shared.MicroserviceSchema
+{
+    public class StringResponse : BaseResponse
+    {
+        public readonly string value;
+
+        public StringResponse(string value) : base(true)
+        {
+            this.value = value;
+        }
+    }
+}
